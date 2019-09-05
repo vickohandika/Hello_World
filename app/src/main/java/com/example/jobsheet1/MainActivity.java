a7cbaeab72d4db4a5d12fc92ed9f655590d5a88a
vickohandika <vickonan8@gmail.com>
2019-09-05 14:21:43 +0700

    perubahan1

--- app/src/main/java/com/example/jobsheet1/MainActivity.java
+++ app/src/main/java/com/example/jobsheet1/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.jobsheet1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportActionBar().setTitle("Hello World!");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"App on Start",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"App on Stop",Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
+
